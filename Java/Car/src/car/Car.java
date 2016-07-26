0af1a0bf33661518f8c64d8205d2de1b932b3731
rizkiprayoga <rizki.prayoga@gmail.com>
2016-07-26 11:52:29 +0700

    Hackerrank java training 2 completed

--- Java/Car/src/car/Car.java
+++ Java/Car/src/car/Car.java
@@ -5,13 +5,27 @@ public class Car {
 
     int maxSpeed = 100;
     int minSpeed = 0;
+    
     double weight = 4079;
+    
     boolean isTheCarOn = false;
     // char variables use single quote, string variables uses double quote
     char condition = 'A';
     String nameOfCar = "Lucy";
     
-    //public means this is public class
+    double maxFuel = 16;
+    double currentFuel = 8;
+    double mpg = 26.4;
+    
+    int numberOfPeopleInCar = 1;
+    
+    public Car(int customMaxSpeed, double customWeight, boolean customIsTheCarOn){
+        maxSpeed=customMaxSpeed;
+        weight=customWeight;
+        isTheCarOn=customIsTheCarOn;
+    }
+    
+    //public means this is public instances
     //void means it doesn't return anything
     public void printVariables(){
         System.out.println(maxSpeed);
@@ -20,6 +34,7 @@ public class Car {
         System.out.println(isTheCarOn);
         System.out.println(condition);
         System.out.println(nameOfCar);
+        System.out.println(numberOfPeopleInCar);
     }
     
     public void upgradeMinSpeed(){
@@ -27,13 +42,42 @@ public class Car {
         maxSpeed = maxSpeed + 1;
     }
     
+    public void getIn(){
+        //numberOfPeopleInCar = numberOfPeopleInCar + 1;
+        numberOfPeopleInCar++;
+    }
+    
+     public void getOut(){
+        //numberOfPeopleInCar = numberOfPeopleInCar - 1;
+        numberOfPeopleInCar--;
+    }
+    
+    public double howManyMilesTillOutOfGas(){
+        return currentFuel * mpg;
+    }
+    
+    public double maxMilesPerFillUp(){
+        return maxFuel * mpg;
+    }
+    
     public static void main(String[] args) {
         // TODO code application logic here
-        Car familyCar = new Car();
-        System.out.println("Family's Car :");
-        familyCar.printVariables();
-        familyCar.upgradeMinSpeed();
-        familyCar.printVariables();
+        Car birthdayPresent = new Car(200, 5000, true);
+        System.out.println("Birthday Car V1 : ");
+        birthdayPresent.printVariables();
+//        System.out.println("Christmas Present:");
+//        Car christmasPresent = new Car (400, 2000, false);
+//        christmasPresent.printVariables();
+        System.out.println("Birthday Car V2 : ");
+        birthdayPresent.getIn();
+        birthdayPresent.getIn();
+        birthdayPresent.getIn();
+        birthdayPresent.printVariables();
+        System.out.println("How Many Miles Left : " + birthdayPresent.howManyMilesTillOutOfGas());
+        System.out.println("Max Miles : " + birthdayPresent.maxMilesPerFillUp());
+        birthdayPresent.getOut();
+        System.out.println("Birthday Car V2 : ");
+        birthdayPresent.printVariables();
     }
     
 }
